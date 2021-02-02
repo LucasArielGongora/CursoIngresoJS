08de962ecb1b259c62c1d001cea53fd04c89c68d
LucasArielGongora <60543911+LucasArielGongora@users.noreply.github.com>
2021-02-02 15:18:46 -0300

    IF modificaion 1

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
-//tomo la edad  
+	var SuEdad;
 
+	SuEdad=document.getElementById("edad").value;
 
-
+	if(SuEdad=15)
+	{
+		alert("Niña bonita");
+	}else{
+			alert("intente otra vez");
+		}
+	
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
-//tomo la edad  
+	//tomo la edad  
+	let Edad;
 
+	edad= document.getElementById("edad").value;
+
+	if(edad>=18)
+	{
+		console.log("es mayor de edad");
+	}else if (edad<18)&&(edad>0)
+			{
+				console.log("es menor de edad");
+			}
 
 
 }//FIN DE LA FUNCIÓN
